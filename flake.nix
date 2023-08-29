c22c00938db86a1c17a32aea6c44ae622131ba6e
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2023-08-29 08:14:51 -0400

    Adding flake files

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.cargo-watch
+          pkgs.iconv
+          pkgs.just
+          pkgs.nerdfonts
+          pkgs.rustup
+          pkgs.starship
+          pkgs.trunk
+        ];
+        shellHook = ''
+          source .env
+          rustup default stable
+          rustup target add wasm32-unknown-unknown
+          eval "$(starship init bash)"
+        '';
+      };
+    });
+}
